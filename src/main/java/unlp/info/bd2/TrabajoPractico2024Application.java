de378bd69369b4faffe0e70cb01586578680321d
Relderf <relderfeg@gmail.com>
2024-04-05 20:56:52 -0300

    feat(all)!: se agrega el proyecto provisto por la cátedra

---
+++ src/main/java/unlp/info/bd2/TrabajoPractico2024Application.java
@@ -0,0 +1,13 @@
+package unlp.info.bd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoPractico2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoPractico2024Application.class, args);
+	}
+
+}
