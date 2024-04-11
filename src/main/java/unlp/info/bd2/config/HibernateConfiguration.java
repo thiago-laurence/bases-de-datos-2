d45394584ddd52cb4afa0ffc5d82571fb5e8a76e
thiago-laurence <francolaurence47@gmail.com>
2024-04-10 22:35:55 -0300

    fix(maven-bd): se arreglo dependencias maven, driver de conexion bd y credenciales de acceso.

--- src/main/java/unlp/info/bd2/config/HibernateConfiguration.java
+++ src/main/java/unlp/info/bd2/config/HibernateConfiguration.java
@@ -30,7 +30,7 @@ public class HibernateConfiguration {
     @Bean
     public DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/bd2_tours_" + this.getGroupNumber() +"?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true&useTimezone=true&serverTimezone=UTC");
         dataSource.setUsername("AdminGrupo5");
         dataSource.setPassword("AdminGrupo5Password");
@@ -57,7 +57,7 @@ public class HibernateConfiguration {
     }
 
     private int getGroupNumber(){
-        return 10;
+        return 5;
     }
 
 }
