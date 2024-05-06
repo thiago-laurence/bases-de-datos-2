532162d62263617db1a0f8e665a34cda023a0d9c
Relderf <relderfeg@gmail.com>
2024-05-06 08:58:56 -0300

    fix: Agregado CascadeType.Persist a Purchase para arreglar problema de arranque initializer

--- src/main/java/unlp/info/bd2/model/Purchase.java
+++ src/main/java/unlp/info/bd2/model/Purchase.java
@@ -28,7 +28,7 @@ public class Purchase {
     @JoinColumn(name = "route_id", nullable = false)
     private Route route;
 
-    @OneToOne(fetch = FetchType.EAGER, mappedBy = "purchase", cascade = { CascadeType.REMOVE })
+    @OneToOne(fetch = FetchType.EAGER, mappedBy = "purchase", cascade = { CascadeType.REMOVE, CascadeType.PERSIST })
     private Review review;
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "purchase", cascade = { CascadeType.REMOVE }, orphanRemoval = true)
